a632c76cc2620be1973c0c461b916ba224227137
Bruno Ricardo <brunoricardosecco@hotmail.com>
2020-04-08 21:23:16 -0300

    refactor: move all error imports toa  single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,5 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { EmailValidator } from '../protocols/emailValidator'
 
 import { badRequest, serverError } from '../helpers/http-helper'
